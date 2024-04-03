4741613ace2da7096d393d129aacdd261df1aba1
vanessa <escavanessa@gmail.com>
2024-04-02 20:21:53 -0400

    moving to right now working

--- EnemyController.js
+++ EnemyController.js
@@ -1,4 +1,5 @@
 import Enemy from "./Enemy.js";
+import MovingDirection from "./MovingDirection.js";
 
 export default class EnemyController {
     enemyMap = [
@@ -12,17 +13,40 @@ export default class EnemyController {
 
       enemyRows = [];
 
+
+    currenDirection = MovingDirection.right;
+    xVelocity = 0;
+    yVelocity = 0;
+    defaultXVelocity = 1;
+    defaultYVelocity = 1;
+
     constructor(canvas) {
         this.canvas = canvas;
         this.createEnemies();
     }
 
     draw(ctx) {
+        this.updateVelocityAndDirection
         this.drawEnemies(ctx);
+    }
+
+    updateVelocityAndDirection() {
+        for(const enemyRow of this.enemyRows) {
+            if(this.currenDirection == MovingDirection.right) {
+                this.xVelocity = this.defaultXVelocity;
+                this.yVelocity = 0;
+                const rightMostEnemy = enemyRow[enemyRow.length -1];
+                if(rightMostEnemy.x + rightMostEnemy.width >= this.canvas.width){
+                    this.currenDirection = MovingDirection.downLeft;
+                    break;
+                }
+            }
+        }
     }
     //tells each enemy to draw itself
     drawEnemies(ctx) {
         this.enemyRows.flat().forEach((enemy) => {
+            enemy.move(this.xVelocity, this.yVelocity)
             enemy.draw(ctx)
         })
     }
